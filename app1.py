df640c71c4bf88061659f66dd307c1e0bb5d8bb7
Tejashwar Senthil Kumar <59280346+tejashwarskumar@users.noreply.github.com>
2024-08-29 20:38:39 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -10,6 +10,10 @@ os.environ["GOOGLE_API_KEY"] = "AIzaSyDwmsQ-9hJTZV3ig0jioi2ZIAZxK3x9ras"
 import streamlit as st
 import pandas as pd
 import os
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import time
 
 file_formats = {
     "csv": pd.read_csv,
@@ -74,24 +78,53 @@ if prompt := st.chat_input(placeholder="Enter your query"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    if "success metric" in prompt:
-        df_to_use = df2
+    if "?" not in prompt:
+        
+        paragraph = """4 experiments led to an increase in TPre trial initiations.
+                        7 copy experiments were rolled out in July.
+                        The average duration for an experiment to see statistically significant results is 14 days.
+                        The commonly used success metrics used by TPre experimenters are "Users clicking on the button" and "Users who started the trial".
+                        The guidelines for identifying relevant success metrics for your experiment are that they should align with business goals, be quantifiable and measurable, be sensitive to changes and consistent across flights.
+                        The common guardrail conditions for growth experiments are that the Core metrics should not exhibit any regressions in Teams features.
+                        To design an experiment to test the performance of x copy variants the success metrics should be Click Through Rate for the feature copy.
+                        Even if the metrics show no negative movements, the experiment can be considered as successful only if the movement in the metrics is statistically significant.
+                    """
+        
+        sentences = nltk.sent_tokenize(paragraph)
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(sentences)
+
+        def find_best_answer(question):
+            question_tfidf = vectorizer.transform([question])
+            similarities = cosine_similarity(question_tfidf, tfidf_matrix).flatten()
+            best_match_index = similarities.argmax()
+            return sentences[best_match_index]
+
+        best_answer = find_best_answer(prompt)
+        with st.chat_message("assistant"):
+            time.sleep(2)
+            st.session_state.messages.append({"role": "assistant", "content": best_answer})
+            st.write(best_answer)
+        
     else:
-        df_to_use = df
-
-    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.2, streaming=True)
-
-    pandas_df_agent = create_pandas_dataframe_agent(
-        llm,
-        df_to_use,
-        verbose=True,
-        agent_type="zero-shot-react-description",
-        handle_parsing_errors=True,
-        allow_dangerous_code=True
-    )
-
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        st.write(response)
+        if "success metric" in prompt:
+            df_to_use = df2
+        else:
+            df_to_use = df
+    
+        llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.2, streaming=True)
+    
+        pandas_df_agent = create_pandas_dataframe_agent(
+            llm,
+            df_to_use,
+            verbose=True,
+            agent_type="zero-shot-react-description",
+            handle_parsing_errors=True,
+            allow_dangerous_code=True
+        )
+    
+        with st.chat_message("assistant"):
+            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            st.write(response)
